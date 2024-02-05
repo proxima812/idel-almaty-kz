1658c1c9673eb8951f7cbdad6fe63958a3ab8237
proxima812 <mrzuc2015@yandex.ru>
2024-02-05 22:49:43 +0600

    asdas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import { defineConfig } from "astro/config"
 import markdoc from "@astrojs/markdoc"
+import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
+import { defineConfig } from "astro/config"
 
 export default defineConfig({
 	site: "https://idel-almaty.kz/",
